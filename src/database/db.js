af65a1434bf8ccaafd6492087ea97ec1a09f5dd5
Yamidor <41703136+Yamidor@users.noreply.github.com>
2023-06-30 17:52:48 -0500

    pool2

--- src/database/db.js
+++ src/database/db.js
@@ -4,15 +4,17 @@ const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,{
     host: DB_HOST,
     dialect: 'mysql',
     dialectOptions: {
-        connectTimeout:100000
+        connectTimeout:60000
     },
     define: {
         timestamps: false
     },
     pool: {
-        max: 25,
+        max: 5,
         min: 0,
-        idle: 10000
+        acquire: 30000, // Tiempo máximo de espera para obtener una conexión (30 segundos)
+        idle: 10000, // Tiempo máximo de inactividad de una conexión antes de que se cierre (10 segundos)
+        evict: 60000,
     }
 
 })
