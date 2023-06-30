631fbe824aaf7ef7f51a77cf1da116190664ac0f
Yamidor <41703136+Yamidor@users.noreply.github.com>
2023-06-30 17:12:58 -0500

    pool

--- src/database/db.js
+++ src/database/db.js
@@ -3,8 +3,16 @@ import {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST} from './config.js'
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,{
     host: DB_HOST,
     dialect: 'mysql',
+    dialectOptions: {
+        connectTimeout:100000
+    },
     define: {
         timestamps: false
+    },
+    pool: {
+        max: 25,
+        min: 0,
+        idle: 10000
     }
 
 })
